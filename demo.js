d60877e95434d9c14fc79d671b34413b5c62dd2c
Suparya24 <suparya.das@hirepro.in>
2016-02-12 13:19:20 +0530

    demo.js
    my first program in git repo

---
+++ demo.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head><title></title></head>
+<body>
+
+<h2>My 1st Java Script program in git repo</h2>
+
+<p id="demo">Print Hello java script</p>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">
+Click Me!</button>
+
+</body>
+</html>
